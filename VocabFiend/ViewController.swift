16ed0cc920c3be8a1b96fc074f3523aab32748cc
Joe Smith <jsmith@twitter.com>
2015-03-15 17:19:07 -0700

    Initial Commit

---
+++ VocabFiend/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  VocabFiend
+//
+//  Created by Joseph Smith on 3/15/15.
+//  Copyright (c) 2015 bjoli. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
